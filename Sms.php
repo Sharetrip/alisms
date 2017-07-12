c547d8a44e1c2c563199902ce7e85e986738ca30
YunFanChen <sharetrip@qq.com>
2017-07-12 16:52:44 +0800

    更新

--- Sms.php
+++ Sms.php
@@ -97,6 +97,40 @@ class Sms
 
     }
 
+	public function querySendDetails() {
+    
+    //此处需要替换成自己的AK信息
+    $accessKeyId = "yourAccessKeyId";
+    $accessKeySecret = "yourAccessKeySecret";
+    //短信API产品名
+    $product = "Dysmsapi";
+    //短信API产品域名
+    $domain = "dysmsapi.aliyuncs.com";
+    //暂时不支持多Region
+    $region = "cn-hangzhou";
+    
+    //初始化访问的acsCleint
+    $profile = DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
+    DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
+    $acsClient= new DefaultAcsClient($profile);
+    
+    $request = new Dysmsapi\Request\V20170525\QuerySendDetailsRequest();
+    //必填-短信接收号码
+    $request->setPhoneNumber("15000000000");
+    //选填-短信发送流水号
+    $request->setBizId("abcdefgh");
+    //必填-短信发送日期，支持近30天记录查询，格式yyyyMMdd
+    $request->setSendDate("20170525");
+    //必填-分页大小
+    $request->setPageSize(10);
+    //必填-当前页码
+    $request->setContent(1);
+    
+    //发起访问请求
+    $acsResponse = $acsClient->getAcsResponse($request);
+    var_dump($acsResponse);
+    
+}
 
 
 
